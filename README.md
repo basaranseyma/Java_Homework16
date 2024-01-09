981d5d25ae957ea31aea771e675e082665cb511e
Seyma Basaran <basaranseyma@gmail.com>
2024-01-09 07:03:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Leap Year Checker 📅 
+This program is an assignment for [Patika.dev](https://patika.dev), and it checks if a given year is a **leap year** or not. A leap year is determined based on the following conditions:
+
+* If a year is divisible by 4 but not divisible by 100, it's a leap year.
+* If a year is divisible by 400, it's also a leap year.
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the year: ");
+        int year = input.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " isn't a leap year.");
+        }
+    }
+}
